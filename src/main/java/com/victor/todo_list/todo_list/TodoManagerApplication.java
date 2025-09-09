6b94ed8abcef3e57275c614dd6e4478e46bfb2a8
v-filgueiras <vfilgueiras.dev@gmail.com>
2025-09-09 06:51:07 -0400

    upando arquivos para o repositorio

---
+++ src/main/java/com/victor/todo_list/todo_list/TodoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.victor.todo_list.todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/todo_list/todo_list/TodoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.todo_list.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
