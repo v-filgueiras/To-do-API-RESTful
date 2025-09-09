8d33cf7ffb0d690b025d60d8b818feae1149a338
v-filgueiras <vfilgueiras.dev@gmail.com>
2025-09-09 07:20:10 -0400

    adicionando tags do JPA Repository

--- src/main/java/com/victor/todo_list/todo_list/domain/entity/UserEntity.java
+++ src/main/java/com/victor/todo_list/todo_list/domain/entity/UserEntity.java
@@ -1,9 +1,30 @@
 package com.victor.todo_list.todo_list.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table 
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 100)
     private String name;
+
+    @Column(name = "age", nullable = false)
     private int age;
+    
+    @Column(name = "cpf", nullable = false)
     private String cpf;
+
+    @Column(name = "e-mail", nullable = false, length = 124)
     private String email;
 
     public UserEntity(String name, int age, String cpf, String email) {
